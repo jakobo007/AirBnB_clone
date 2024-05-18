b2339a7b986d6f8286967e85fc710453105d23b9
Ouma James Joshua <122970072+jakobo007@users.noreply.github.com>
2024-05-18 17:16:12 +0300

    changes8

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
     def do_quit(self, arg):
         """Quit command to exit the program"""
+        print()
         return True
 
     def do_EOF(self, arg):
